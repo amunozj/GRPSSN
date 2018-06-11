debf4f8e2a46c5c090cd44b2f395e4ae31f8b8bc
Sam Hollenbach <shollenb@macalester.edu>
2018-06-11 15:44:25 -0600

    Added new Jupyter notebook

--- scripts/ADF_SSN_03_ADF_all_Observers.py
+++ scripts/ADF_SSN_03_ADF_all_Observers.py
@@ -71,9 +71,9 @@ if args.start_id is not None:
 if args.end_id is not None:
     SSN_ADF_Config.OBS_END_ID = args.end_id
 
+# Flag to skip over already processed observers (see config file for more detail)
 if args.skip_observers:
     SSN_ADF_Config.SKIP_OBSERVERS_WITH_PLOTS = args.skip_observers
-
 
 #################
 # STARTING SCRIPT#
@@ -128,7 +128,7 @@ def run_obs(CalObs):
     if CalObs in SSN_ADF_Config.SKIP_OBS:
         return
 
-    print("######## Beginning run on observer {} ########\n".format(CalObs))
+    print("######## Starting run on observer {} ########\n".format(CalObs))
 
     # Processing observer
     obs_valid = ssn_adf.processObserver(ssn_data,  # SSN metadata
@@ -217,6 +217,8 @@ if __name__ == '__main__':
 
     obs_range = range(SSN_ADF_Config.OBS_START_ID, SSN_ADF_Config.OBS_END_ID)
 
+    # If SKIP_OBSERVERS_WITH_PLOTS is set, for each observer find matching directory
+    # If there is a file with the same flags as currently set, add this observer to list of observers to skip
     if SSN_ADF_Config.SKIP_OBSERVERS_WITH_PLOTS:
         out_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'output', 'TestFrag')
         for ob in obs_range:
@@ -229,23 +231,24 @@ if __name__ == '__main__':
                             break
         print("\nSkipping observers who have plot(s) labeled with the current flags ({} / {}):\n{}\n"
               "Change the SKIP_OBSERVERS_WITH_PLOTS config flag to remove this behavior\n".format(
-            SSN_ADF_Config.ADF_TYPE, SSN_ADF_Config.MONTH_TYPE,SSN_ADF_Config.SKIP_OBS))
+            SSN_ADF_Config.ADF_TYPE, SSN_ADF_Config.MONTH_TYPE, SSN_ADF_Config.SKIP_OBS))
 
     if SSN_ADF_Config.PROCESSES == 1:
         for i in obs_range:
             run_obs(i)
     elif SSN_ADF_Config.PROCESSES == -1:
         try:
-            pool = Pool()  # Create a multiprocessing Pool
+            pool = Pool()  # Create a multiprocessing Pool with all available cores
             pool.map(run_obs, obs_range)  # process all observer iterable with pool
         finally:  # To make sure processes are closed in the end, even if errors happen
             pool.close()
             pool.join()
     else:
+        # Safety checks for process number
         if SSN_ADF_Config.PROCESSES < -1 or SSN_ADF_Config.PROCESSES == 0:
             raise ValueError(
                 "Invalid processes number ({}). Please set to a valid number.".format(SSN_ADF_Config.PROCESSES))
-        if SSN_ADF_Config.PROCESSES > os.cpu_count():
+        elif SSN_ADF_Config.PROCESSES > os.cpu_count():
             raise ValueError("Processes number higher than CPU count. "
                              "You tried to initiate {} processes, while only having {} CPU's.".format(
                 SSN_ADF_Config.PROCESSES, os.cpu_count()))
